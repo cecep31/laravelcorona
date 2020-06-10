24d68f3bf5f4bf3a61ac4f6d2fdd1c702cc93d26
cecep31 <cecepjanuardi31@gmail.com>
2020-06-10 14:52:17 +0700

    cek commit vs code

---
+++ resources/views/coba.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>berada di beta</title>
+</head>
+<body>
+    {{
+    for ($i=0; $i < 5 ; $i++) {
+        echo "belajar dulu";
+    }
+    }}
+
+</body>
+</html>
